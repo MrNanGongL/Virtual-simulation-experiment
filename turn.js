b85e060f436d6535e408b5bff07b3032aed34a93
MrNanGongL <53283398+MrNanGongL@users.noreply.github.com>
2019-07-25 13:41:49 +0800

    Delete turn.js

--- turn.js
+++
@@ -1,8 +0,0 @@
-function gohome(){
-	window.location.href="./index.html"}//首页
-
-function goland(){
-	window.location.href="./land.html"}//登陆
-
-function gozland(){
-	window.location.href="./zland.html"}//注册
