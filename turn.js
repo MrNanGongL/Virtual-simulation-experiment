289d7203ef6f689ee099ad2ca0a85ac08592e511
MrNanGongL <53283398+MrNanGongL@users.noreply.github.com>
2019-07-25 11:14:46 +0800

    Create turn.js

---
+++ turn.js
@@ -0,0 +1,8 @@
+function gohome(){
+	window.location.href="./index.html"}//首页
+
+function goland(){
+	window.location.href="./land.html"}//登陆
+
+function gozland(){
+	window.location.href="./zland.html"}//注册
